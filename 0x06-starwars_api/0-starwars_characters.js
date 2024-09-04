cc3664a7841b0dd473b691d984e7f13d02695733
olaoluwiv <ayidinao@gmail.com>
2024-09-04 11:29:09 +0000

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,52 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-const filmId = process.argv[2];
-const filmAPI = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-let people = [];
-const names = [];
 
-const charactersAPI = async () => {
-  await new Promise(resolve => request(filmAPI, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      console.error('Error: ', error, '| StatusCode: ', response.statusCode);
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
     } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
-  }));
+  });
 };
 
-const characterNames = async () => {
-  if (people.length > 0) {
-    for (const person of people) {
-      await new Promise(resolve => request(person, (error, response, body) => {
-        if (error || response.statusCode !== 200) {
-          console.error('Error: ', error, '| StatusCode: ', response.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no characters for some reason');
-  }
-};
-
-const getCharacterNames = async () => {
-  await charactersAPI();
-  await characterNames();
-
-  for (const name of names) {
-    if (name === names[names.length - 1]) {
-      process.stdout.write(name);
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
     } else {
-      process.stdout.write(name + '\n');
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
     }
   }
-};
-
-getCharacterNames();
+);
